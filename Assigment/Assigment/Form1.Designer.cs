5c81bdfc4daa1b8bc327ae698b801d88282f147d
fowzia354 <fowziajiiaaye@gmail.com>
2026-09-24 18:24:29 +0300

    update

---
+++ Assigment/Assigment/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Assigment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbldatoutput = new System.Windows.Forms.Label();
+            this.txtdayoftheWeek = new System.Windows.Forms.TextBox();
+            this.txtdayofthemonth = new System.Windows.Forms.TextBox();
+            this.dayofmonthtextbox = new System.Windows.Forms.TextBox();
+            this.txtyear = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter the day of the week";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter the name of the month";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(130, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(258, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Enter the numeric day of the month";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(130, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Enter the year";
+            // 
+            // lbldatoutput
+            // 
+            this.lbldatoutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbldatoutput.Location = new System.Drawing.Point(87, 278);
+            this.lbldatoutput.Name = "lbldatoutput";
+            this.lbldatoutput.Size = new System.Drawing.Size(642, 43);
+            this.lbldatoutput.TabIndex = 5;
+            // 
+            // txtdayoftheWeek
+            // 
+            this.txtdayoftheWeek.Location = new System.Drawing.Point(496, 33);
+            this.txtdayoftheWeek.Name = "txtdayoftheWeek";
+            this.txtdayoftheWeek.Size = new System.Drawing.Size(164, 26);
+            this.txtdayoftheWeek.TabIndex = 6;
+            // 
+            // txtdayofthemonth
+            // 
+            this.txtdayofthemonth.Location = new System.Drawing.Point(496, 78);
+            this.txtdayofthemonth.Name = "txtdayofthemonth";
+            this.txtdayofthemonth.Size = new System.Drawing.Size(164, 26);
+            this.txtdayofthemonth.TabIndex = 7;
+            // 
+            // dayofmonthtextbox
+            // 
+            this.dayofmonthtextbox.Location = new System.Drawing.Point(496, 127);
+            this.dayofmonthtextbox.Name = "dayofmonthtextbox";
+            this.dayofmonthtextbox.Size = new System.Drawing.Size(164, 26);
+            this.dayofmonthtextbox.TabIndex = 8;
+            // 
+            // txtyear
+            // 
+            this.txtyear.Location = new System.Drawing.Point(496, 196);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(164, 26);
+            this.txtyear.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "show date";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(327, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 30);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(603, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 33);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtyear);
+            this.Controls.Add(this.dayofmonthtextbox);
+            this.Controls.Add(this.txtdayofthemonth);
+            this.Controls.Add(this.txtdayoftheWeek);
+            this.Controls.Add(this.lbldatoutput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbldatoutput;
+        private System.Windows.Forms.TextBox txtdayoftheWeek;
+        private System.Windows.Forms.TextBox txtdayofthemonth;
+        private System.Windows.Forms.TextBox dayofmonthtextbox;
+        private System.Windows.Forms.TextBox txtyear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Assigment/Assigment/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assigment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Creating variables to store input data
+
+            string DayoftheWeek, Month, Day, Year, FullDate;
+
+            //Assign variable to user input
+
+            DayoftheWeek = txtdayoftheWeek.Text;
+
+            Month = txtdayofthemonth.Text;
+
+            Day = dayofmonthtextbox.Text;
+
+            Year = txtyear.Text;
+
+            //process using concatination
+
+            FullDate = DayoftheWeek  +Month + "" + Day + "," + Year;
+
+            //Display The output
+
+            lbldatoutput.Text = FullDate;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //clear textbox and label
+
+            txtdayoftheWeek.Text = "";
+
+txtdayofthemonth.Clear();
+
+            dayofmonthtextbox.Text = string.Empty;
+
+            txtyear.Text = string.Empty;
+
+            lbldatoutput.Text = string.Empty;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
